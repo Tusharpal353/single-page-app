a9a73ea35d137c9796bf2b7b994734edc93a51a7
Tusharpal353 <93274939+Tusharpal353@users.noreply.github.com>
2022-08-13 22:00:52 +0530

    Add files via upload

---
+++ React-OnepageApp-master/React-OnepageApp-master/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Footer from './components/Footer';
+import Form from './components/Form';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Navbar></Navbar>
+      <Form ></Form>
+      <Footer></Footer>
+      </>
+    </div>
+  );
+}
+
+export default App;
